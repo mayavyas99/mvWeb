ae2d7a580beaf2ecec0bc78941e2780710796c92
mayavyas99 <mayavyasrun@gmail.com>
2025-02-18 00:07:14 -0500

    Delete docs directory

--- docs/Helpers/helper.js
+++
@@ -1,6 +0,0 @@
-// Function to get elements by matching part of the title attribute
-function getElementsByTitlePattern(pattern) {
-    return Array.from(document.querySelectorAll('area')).filter(area =>
-        area.getAttribute('title').toLowerCase().includes(pattern.toLowerCase())
-    );
-}
